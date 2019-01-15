23c6a4b114e432db2216f5c7b34eb602183de6b2
Vivek <akellavivek1999@gmail.com>
2019-01-16 01:19:31 +0530

    Default fragment added

--- app/src/main/java/com/example/pratik/digitaloutpass/MainActivity.java
+++ app/src/main/java/com/example/pratik/digitaloutpass/MainActivity.java
@@ -40,6 +40,8 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        LoginStudentFragment fragment = LoginStudentFragment.newInstance("1", "2");
     }
 
     @Override
